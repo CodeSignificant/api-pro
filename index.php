9cb119f842ebef93e196c4371587475f1a1d2c7d
CodeSignificant <108189881+CodeSignificant@users.noreply.github.com>
2026-01-05 20:11:40 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once __DIR__ . '/api-pro/autoload.php';
-require_once __DIR__ . '/vendor/autoload.php';
+// require_once __DIR__ . '/vendor/autoload.php'; Uncomment when other packages used
 
 require_once __DIR__ . '/lib/services/HomeService.php';
 
@@ -9,4 +9,5 @@ $homeService = ProNode::Service('/v1/home', new HomeService());
 $homeService->get('/hello', 'hello');
 
 ProNode::start();
+
 
