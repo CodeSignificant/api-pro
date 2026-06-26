b93c00371ffdedb1536014311988331761bbe1d2
CodeSignificant <4ss.software.adi@gmail.com>
2026-06-27 00:46:35 +0530

    Update default CORS origins in config.php

--- config.php
+++ config.php
@@ -27,7 +27,5 @@ define('TOKEN_MULTIPLE_DEVICE_LOGIN', true);
 define('TOKEN_MAX_DEVICES', 5);
 define('TOKEN_ALLOW_CONCURRENT', true);
 define('SESSION_TIME', 1800);
-define('DB_WRITE', 'update');                     // Global DB schema sync mode: 'create', 'force', 'update', or false to disable
-
-// Allowed CORS Origins (comma-separated list of domains)
-define("CORS", "http://localhost:3000, http://127.0.0.1:3000, https://crm.4ss.in, https://crm.doland.in");
+define('DB_WRITE', 'update');                   
+define("CORS", "http://localhost:3000, http://127.0.0.1:3000");
